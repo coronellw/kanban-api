f8935ca68ce33b66f75e56657aff33c977616bd7
Wiston Coronell <coronell@gmail.com>
2025-01-12 14:58:36 -0400

    - Removed unused endpoints - populated tasks and subtasks when querying boards

--- src/routers/boards.ts
+++ src/routers/boards.ts
@@ -9,25 +9,17 @@ router.post('/boards', auth, async (req: UserRequest, res: UserResponse) => {
   try {
     const board = new Board({ ...req.body, owner: req.user._id })
     await board.save()
-    res.send(board)
+    res.status(201).send(board)
   } catch (error) {
     res.status(400).send(error)
   }
 })
 
-router.post('/boards/:id/columns', auth, checkBoard, async (req: UserRequest, res: UserResponse) => {
-  try {
-    req.board.columns.push(req.body)
-    await req.board.save()
-    res.send(req.board)
-  } catch (error) {
-    res.status(500).send(error)
-  }
-})
-
 router.get('/boards', auth, async (req: UserRequest, res: UserResponse) => {
   try {
-    const boards = await Board.find({ owner: req.user._id }).populate('columns').populate('owner').exec()
+    const boards = await Board.find({ owner: req.user._id })
+    .populate({path: 'columns', populate: {path: 'tasks'}})
+    .populate('owner').exec()
     res.send(boards)
   } catch (error) {
     res.status(500).send(error)
@@ -49,16 +41,6 @@ router.patch('/boards/:id', auth, checkBoard, async (req: UserRequest, res: User
     const board = applyUpdates(req.board, req.body)
     await board.save()
     res.status(202).send(board)
-  } catch (error) {
-    res.status(500).send(error)
-  }
-})
-
-router.delete('/boards/:id/columns', auth, checkBoard, async (req: UserRequest, res: UserResponse) => {
-  try {
-    await req.board.columns.id(req.body.boardId).deleteOne()
-    await req.board.save()
-    res.send(req.board)
   } catch (error) {
     res.status(500).send(error)
   }
