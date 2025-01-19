03da61be1ae847882dd328642f514ac0222687bb
Wiston Coronell <coronell@gmail.com>
2025-01-19 00:51:05 -0400

    removing dist from tracking

--- dist/utils/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.isSubsetOf = void 0;
-const isSubsetOf = (list, target) => list.every(item => target.includes(item));
-exports.isSubsetOf = isSubsetOf;
-//# sourceMappingURL=index.js.map
